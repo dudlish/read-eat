0c1fceeff84d647ced2098d7bcef4f10198a5261
Katja Makarova <47348068+dudlish@users.noreply.github.com>
2020-07-15 14:46:06 +0300

    Add bootstrap (#1)
    * Changed class names in navbar, intro, editor's choice, lightbox images
    
    * Lightbox class names are changed in html, css and js
    
    * Lightbox class names are changed in html, css and js
    
    * Bootstrap Navbar added
    
    * Old Navbar and its css deleted
    
    * Jumbotron added; intro class, its contents and styling removed
    
    * Jumbotron added; intro class, its contents and styling removed
    
    * Editor's choice recipe header transformed using Bootstrap
    
    * Editor's choice recipe block changed from several rows to one
    
    * Blog posts added
    
    * Share a recipe form added
    
    * Buttons customized
    
    * Genres added
    
    * Responsive style added to genres and blog
    
    * Images

--- lightbox.js
+++ lightbox.js
@@ -1,45 +1,45 @@
 // Open the Modal
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
+function openLightboxModal() {
+    document.getElementById("lightbox-modal").style.display = "block";
 }
 
 // Close the Modal
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
+function closeLightboxModal() {
+    document.getElementById("lightbox-modal").style.display = "none";
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+let lightboxSlideIndex = 1;
+lightboxShowSlides(lightboxSlideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function lightboxPlusSlides(n) {
+    lightboxShowSlides(lightboxSlideIndex += n);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function lightboxCurrentSlide(n) {
+    lightboxShowSlides(lightboxSlideIndex = n);
 }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+function lightboxShowSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("lightboxSlides");
+    let dots = document.getElementsByClassName("lightboxDemo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) { lightboxSlideIndex = 1 }
+    if (n < 1) { lightboxSlideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" lightbox-active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
+    slides[lightboxSlideIndex - 1].style.display = "block";
+    dots[lightboxSlideIndex - 1].className += " lightbox-active";
+    captionText.innerHTML = dots[lightboxSlideIndex - 1].alt;
 }
 
-// let slide = document.querySelectorAll(".mySlides")
+// let slide = document.querySelectorAll(".lightboxSlides")
 // slide.addEventListener("click", function () {
 //     alert("Hello World");
 // });
