77babfaf4cbd6cbf63d0ac0a69b6509bff0ae632
Katja Makarova <47348068+dudlish@users.noreply.github.com>
2020-07-17 16:00:20 +0300

    Removed not used lightbox css and js

--- app.js
+++ app.js
@@ -16,3 +16,37 @@
 //         }
 //     }
 // }
+
+// DOM Variables
+const filter = document.querySelector("#filter");
+const card = document.querySelectorAll(".card")
+
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+    // Filter tasks
+    // filter.addEventListener("keyup", filterTasks);
+    card.addEventListener("click", writeSth);
+}
+
+function whiteSth() {
+    console.log(card.values);
+}
+
+
+
+
+// function filterTasks(e) {
+//     const text = e.target.value.toLowerCase();
+//     console.log(text);
+//     // quertselector returns node list and that is why we can use foreach loop
+//     document.querySelectorAll(".card").forEach(function (task) {
+//         const item = task.firstChild.textContent;
+//         if (item.toLowerCase().indexOf(text) != -1) {
+//             card.style.display = "block";
+//         } else {
+//             card.style.display = "none";
+//         }
+//     });
+// }
