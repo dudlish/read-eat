3c7351accc9c07ba9d424a682c9e06d15b45fa90
Katja Makarova <47348068+dudlish@users.noreply.github.com>
2020-07-18 11:07:44 +0300

    Bootstrap fixes (#2)
    * HTML code cleaned up; comments added
    
    * CSS code cleaned up; Bootstrap footer added and styled
    
    * Adding @media for tablet and mobile
    
    * Added inner links to pages about, recipes, books, blog. HTML pages of these are added
    
    * Inner links at recipes.html added
    
    * Recipe page added and styled
    
    * Stopped the carousel; minor code cleaning
    
    * books.html content added
    
    * Minor changes in books.html structure
    
    * Book genres on index.html and books.html changed from cards to a links. Styling adjusted
    
    * Removed blue border in book genres, text-decoration in the footer
    
    * Horizontal scrolling implemented at recipes.html using Flexbox
    
    * Btns fixed at recipes.html page
    
    * Removed not used lightbox css and js
    
    * books.html transformed all div card into a links
    
    * Team pics added; About oage created
    
    * Post One page added
    
    * Post One styled
    
    * CSS code is cleaned up
    
    * Removed duplicated btns of genres and meals
    
    * Added food pics; changed pics on recipes page and index; styled pics
    
    * Responsive design implementation

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-// // Dropdown menu
-// function dropDownToggle() {
-//     document.getElementById("recipesDropdown").classList.toggle("show");
+// DOM Variables
+// const filter = document.querySelector("#filter");
+// const card = document.querySelectorAll(".card");
+
+// Event Listeners
+// aLink.addEventListener("click", check);
+// cardA.addEventListener("click", myFunc);
+
+// filter.addEventListener("keyup", filterBooks);
+
+// function myFunc() {
+//     console.log(cardA.length);
 // }
 
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function (event) {
-//     if (!event.target.matches('.dropbtn')) {
-//         let dropdowns = document.getElementsByClassName("dropdown-content");
-//         let i;
-//         for (i = 0; i < dropdowns.length; i++) {
-//             let openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains('show')) {
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
-//     }
+// document.querySelectorAll('.card').forEach(item => {
+//     item.addEventListener('click', event => {
+//         console.log(item);
+//     })
+// })
+
+// function filterBooks(e) {
+//     const text = e.target.value.toLowerCase();
+//     console.log(text);
+
+
+    // queryselector returns node list and that is why we can use foreach loop
+    // document.querySelectorAll(".card").forEach(function (book) {
+    //     const item = book.firstChild.textContent;
+
+    //     if (item.toLowerCase().indexOf(text) != -1) {
+    //         card.style.display = "block";
+    //     } else {
+    //         card.style.display = "none";
+    //     }
+    // });
 // }
