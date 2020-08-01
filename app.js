1a4780e0fb3c2f5db3078b0366d3abf27ddfc1c6
Katja Makarova <47348068+dudlish@users.noreply.github.com>
2020-08-01 13:18:59 +0300

    Add json (#6)
    * JSON parse for Books Classics
    
    * Books are filtered by categories and added to the page dynamically
    
    * Books Navbar dropdown menu is created dynamically
    
    * Inner links fixed
    
    * Books JSON added
    
    * Code is cleaned
    
    * Code is cleaned
    
    * Extra space fixed

--- app.js
+++ app.js
@@ -1,36 +1,92 @@
-// DOM Variables
-// const filter = document.querySelector("#filter");
-// const card = document.querySelectorAll(".card");
+function loadJSON(path, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType('application/json');
+    xobj.open('GET', path, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == '200') {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
 
-// Event Listeners
-// aLink.addEventListener("click", check);
-// cardA.addEventListener("click", myFunc);
+function addBook(layoutRoot, book) {
+    const bookDiv = document.createElement('div');
+    bookDiv.className = 'card';
+    bookDiv.id = `book${book.id}`;
 
-// filter.addEventListener("keyup", filterBooks);
+    bookDiv.innerHTML = `
+    <div class="card">
+    <a href="author01.html" class="a-header">
+        <img class="card-img-top" src="${book.img}" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">${book.title}</h5>
+            <p>${book.author}</p>
+        </div>
+    </a>
+    </div>
+    `;
 
-// function myFunc() {
-//     console.log(cardA.length);
-// }
+    layoutRoot.appendChild(bookDiv);
+}
 
-// document.querySelectorAll('.card').forEach(item => {
-//     item.addEventListener('click', event => {
-//         console.log(item);
-//     })
-// })
+function addCategory(layoutRoot, books, category) {
+    console.log(layoutRoot);
+    // Get books of the current category
+    const categoryBooks = books.filter(b => b.category === category);
 
-// function filterBooks(e) {
-//     const text = e.target.value.toLowerCase();
-//     console.log(text);
+    // Create root for current category
+    const categoryRoot = document.createElement('div');
+    categoryRoot.className = "container";
+    categoryRoot.id = `${category.toLowerCase()}`;
+    layoutRoot.appendChild(categoryRoot);
+
+    const hrDotted = categoryRoot.appendChild(document.createElement("hr"));
+    hrDotted.className = "dotted";
+
+    // Add category header
+    const header = document.createElement('h2');
+    header.className = "container";
+    header.id = categoryRoot.id
+    header.innerText = category;
+    categoryRoot.appendChild(header);
+
+    // Add Category Body
+    const categoryBody = document.createElement('div');
+    categoryBody.className = "blog-grid container";
+    categoryRoot.appendChild(categoryBody);
+
+    // Add category books
+    categoryBooks.forEach(book => addBook(categoryBody, book));
+}
 
 
-    // queryselector returns node list and that is why we can use foreach loop
-    // document.querySelectorAll(".card").forEach(function (book) {
-    //     const item = book.firstChild.textContent;
+function addNavBar(navBarRoot, categories) {
+    categories.forEach(category => {
+        const item = document.createElement('a');
 
-    //     if (item.toLowerCase().indexOf(text) != -1) {
-    //         card.style.display = "block";
-    //     } else {
-    //         card.style.display = "none";
-    //     }
-    // });
-// }
+        item.text = category;
+        item.href = `books.html#${category.toLowerCase()}`;
+        item.className = "dropdown-item";
+        navBarRoot.appendChild(item);
+    });
+}
+
+window.onload = () => {
+    const layoutRoot = document.getElementById('books-page');
+    const navBarRoot = document.getElementById('navbarDropdownCategories');
+    console.log(navBarRoot);
+
+    loadJSON('books.json', data => {
+        const books = JSON.parse(data);
+        const categories = new Set(books.map(b => b.category));
+
+        addNavBar(navBarRoot, categories);
+        if (layoutRoot) {
+            categories.forEach(category => {
+                addCategory(layoutRoot, books, category);
+            });
+        }
+    });
+};
